37e4d68871f20b48e620bd8326753f3eb7113125
Med Khalil NASRALLAH <nasrallah.med.khelil@gmail.com>
2020-09-10 22:08:52 +0100

    just for CICD

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json({api:'S2H Secured API' });
+  res.json({api:'S2H Quiz Secured API' });
 });
 
 module.exports = router;
